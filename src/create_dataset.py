58616c6ea69db3bb5272c633ee8d6e39f52a54d5
SS47816 <ss1057532022@gmail.com>
2021-10-28 22:36:19 +0800

    feat: add create_dataset.py

---
+++ src/create_dataset.py
@@ -0,0 +1,57 @@
+import os
+import random
+import shutil
+import numpy as np
+import cv2
+
+def main():
+    # Define paths to the original PIE dataset
+    repo_path = '/Users/ss/ss_ws/face-recognition/'
+    dataset_path = os.path.join(repo_path, 'data/PIE')
+    
+    # Set destination paths
+    train_set_path = os.path.join(repo_path, 'data/train')
+    test_set_path = os.path.join(repo_path, 'data/test')
+    # Remove old folders at the destination (if any)
+    try:
+        shutil.rmtree(train_set_path)
+        shutil.rmtree(test_set_path)
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+    # Create destination folders
+    os.mkdir(train_set_path)
+    os.mkdir(test_set_path)
+
+    # Select 25 from the 68 subjects
+    selected_subjects = random.sample(os.listdir(dataset_path), 25)
+
+    # Within each subject, split into 70-30 train-test set
+    for subject_path in selected_subjects:
+        if subject_path == '.DS_Store':
+            continue
+        img_paths = os.listdir(os.path.join(dataset_path, subject_path))
+        
+        # Split img file into train/test sets
+        train_set = random.sample(img_paths, int(len(img_paths)*0.7))
+        test_set = [item for item in img_paths if item not in train_set]
+        
+        # Copy files to new dataset folders
+        src = os.path.join(dataset_path, subject_path)
+        
+        train_subject_path = os.path.join(train_set_path, subject_path)
+        os.mkdir(train_subject_path)
+        for img_file in train_set:
+            print(os.path.join(src, img_file))
+            print(train_subject_path)
+            shutil.copy2(os.path.join(src, img_file), train_subject_path)
+        
+        test_subject_path = os.path.join(test_set_path, subject_path)
+        os.mkdir(test_subject_path)
+        for img_file in test_set:
+            print(os.path.join(src, img_file))
+            print(test_subject_path)
+            shutil.copy2(os.path.join(src, img_file), test_subject_path)
+
+
+if __name__ == "__main__":
+    main()
