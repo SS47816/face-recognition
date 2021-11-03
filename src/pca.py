82b966af91b4bc4bb4e1557b9ad50d4aea85a92c
SS47816 <ss1057532022@gmail.com>
2021-11-04 02:20:17 +0800

    feat: add new plotting

--- src/pca.py
+++ src/pca.py
@@ -5,8 +5,51 @@ import cv2
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-def readImageData(data_path, set='train', num_PIE_imgs=-1):
+def readImageData(data_path, set='train', num_PIE_imgs=-1) -> tuple:
+    """
+    Read the PIE dataset and my_photo dataset
+
+    Parameters
+    ---
+    `data_path`: `string`, path to the data folder
+    `set`: `string`, can be either `train` or `test`
+    `num_PIE_imgs`: `int`, number of PIE images to sample
+
+    Returns
+    ---
+    `selected_PIE_imgs`: `list[img]`, list of selected PIE img vectors
+    `my_imgs`: `list[img]`, list of selected my_photo img vectors
+    """
+    # List all subjects in set
+    set_path = os.path.join(data_path, set)
+    subject_paths = os.listdir(set_path)
+
+    # Within each subject of the PIE dataset, read all images
+    PIE_imgs = []
+    my_imgs = []
+    for subject_path in subject_paths:
+        folder_path = os.path.join(set_path, subject_path)
+        if subject_path == '.DS_Store':
+            continue
+        elif subject_path == 'my_photo':
+            # Load my_photo images 
+            for img_file in os.listdir(folder_path):
+                my_imgs.append(cv2.imread(os.path.join(folder_path, img_file), cv2.IMREAD_GRAYSCALE).reshape((1, -1)))
+        else:
+            # Load PIE images 
+            for img_file in os.listdir(folder_path):
+                PIE_imgs.append(cv2.imread(os.path.join(folder_path, img_file), cv2.IMREAD_GRAYSCALE).reshape((1, -1)))
     
+    # Randomly Select a given number of samples from the PIE set
+    selected_PIE_imgs = random.sample(PIE_imgs, num_PIE_imgs)
+
+    print('Read %d PIE images from %s' % (len(selected_PIE_imgs), set))
+    print('Read %d my_photo from %s' % (len(my_imgs), set))
+
+    return selected_PIE_imgs, my_imgs
+
+
+def main():
     # Display Settings
     plot_pca_result = True      # If we want to plot the PCA results
     show_num_imgs = 5           # Number of example results to display after done, `0` for no output
@@ -20,6 +63,7 @@ def readImageData(data_path, set='train', num_PIE_imgs=-1):
     # Stack image vectors together
     PIE_train_imgs.extend(my_train_imgs)
     X_train = np.vstack(PIE_train_imgs)
+    img_shape = np.array([np.sqrt(X_train.shape[1]), np.sqrt(X_train.shape[1])], dtype=int)
     print(X_train.shape)
 
     # Apply PCA on 2D and 3D
@@ -50,10 +94,10 @@ def readImageData(data_path, set='train', num_PIE_imgs=-1):
         # Plot the mean face and eigen faces
         fig = plt.figure(figsize=(16, 6))
         ax = fig.add_subplot(1, 4, 1, xticks=[], yticks=[])
-        ax.imshow(pca_3.mean_.reshape((32, 32)), cmap='gray')
+        ax.imshow(pca_3.mean_.reshape(img_shape), cmap='gray')
         for i in range(3):
             ax = fig.add_subplot(1, 4, i + 2, xticks=[], yticks=[])
-            ax.imshow(pca_3.components_[i].reshape((32, 32)), cmap='gray')
+            ax.imshow(pca_3.components_[i].reshape(img_shape), cmap='gray')
         plt.show()
 
     # Apply PCA with 40, 80, and 200 Dimensions
@@ -77,15 +121,15 @@ def readImageData(data_path, set='train', num_PIE_imgs=-1):
         print('Showing %d example results here' % show_num_imgs)
         for i in range(X_train.shape[0]):
             if (i < show_num_imgs):
-                fig, axs = plt.subplots(1, 4)
-                axs[0].title.set_text('Original')
-                axs[0].imshow(X_train[i, :].reshape((32, 32)), cmap='gray')
-                axs[1].title.set_text('D = 40')
-                axs[1].imshow(rec_imgs_list[0][i, :].reshape((32, 32)), cmap='gray')
-                axs[2].title.set_text('D = 80')
-                axs[2].imshow(rec_imgs_list[1][i, :].reshape((32, 32)), cmap='gray')
-                axs[3].title.set_text('D = 200')
-                axs[3].imshow(rec_imgs_list[2][i, :].reshape((32, 32)), cmap='gray')
+                # Plot the original image and the reconstructed faces
+                fig = plt.figure(figsize=(16, 6))
+                ax = fig.add_subplot(1, 4, 1, xticks=[], yticks=[])
+                ax.title.set_text('Original')
+                ax.imshow(X_train[i, :].reshape(img_shape), cmap='gray')
+                for j in range(3):
+                    ax = fig.add_subplot(1, 4, j + 2, xticks=[], yticks=[])
+                    ax.title.set_text('D = %d' %Dimensions[j])
+                    ax.imshow(rec_imgs_list[j][i, :].reshape(img_shape), cmap='gray')
                 plt.show()
             else:
                 break
