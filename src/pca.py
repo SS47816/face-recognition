d8ded34dfb91b4e7cd01d993112e821a64e6d52c
SS47816 <ss1057532022@gmail.com>
2021-11-04 18:21:25 +0800

    feat: pkg plot pca results into a function

--- src/pca.py
+++ src/pca.py
@@ -49,44 +49,28 @@ def readImageData(data_path, set='train', num_PIE_imgs=-1) -> tuple:
 
     return np.vstack(selected_PIE_imgs), np.vstack(my_imgs)
 
-
-def main():
-    # Display Settings
-    plot_pca_result = True      # If we want to plot the PCA results
-    show_num_imgs = 0           # Number of example results to display after done, `0` for no output
-
-    # Set destination paths
-    data_path = '/home/ss/ss_ws/face-recognition/data'
-
-    # Read 500 images from the train set
-    PIE_train, MY_train = readImageData(data_path, set='train', num_PIE_imgs=500)
-
-    # Stack all image vectors together forming X_train set
-    X_train = np.vstack((PIE_train, MY_train))
-    img_shape = np.array([np.sqrt(X_train.shape[1]), np.sqrt(X_train.shape[1])], dtype=int)
-    print(X_train.shape)
-
+def getPCAResults(X_train, PIE_train, MY_train, img_shape, show_plot=True):
     # Apply PCA on 3D (which also included 2D)
     pca_3 = PCA(3)
     pca_3.fit(X_train)
-    proj_PIE_imgs_3d = pca_3.transform(PIE_train)
-    proj_MY_imgs_3d = pca_3.transform(MY_train)
-
-    # Visualize data
-    if plot_pca_result:
+    proj_PIE_imgs = pca_3.transform(PIE_train)
+    proj_MY_imgs = pca_3.transform(MY_train)
+    
+    # Visualize results
+    if show_plot:
         print('Visualizing Results... ')
         plt.style.use('seaborn-whitegrid')
         fig = plt.figure(figsize=plt.figaspect(0.5))
-        # 2D subplot
+        # 2D Plot
         ax = fig.add_subplot(1, 2, 1)
-        ax.scatter(proj_PIE_imgs_3d[:, 0], proj_PIE_imgs_3d[:, 1], s = 15, c = 'c')
-        ax.scatter(proj_MY_imgs_3d[:, 0], proj_MY_imgs_3d[:, 1], s = 15, c = 'r')
+        ax.scatter(proj_PIE_imgs[:, 0], proj_PIE_imgs[:, 1], s = 10, c = 'c')
+        ax.scatter(proj_MY_imgs[:, 0], proj_MY_imgs[:, 1], s = 15, c = 'r')
         ax.set_xlabel('Principle Axis 1')
         ax.set_ylabel('Principle Axis 2')
-        # 3D subplot
+        # 3D Plot
         ax = fig.add_subplot(1, 2, 2, projection='3d')
-        ax.scatter(proj_PIE_imgs_3d[:, 0], proj_PIE_imgs_3d[:, 1], proj_PIE_imgs_3d[:, 2], s = 10, c = 'c')
-        ax.scatter(proj_MY_imgs_3d[:, 0], proj_MY_imgs_3d[:, 1], proj_MY_imgs_3d[:, 2], s = 15, c = 'r')
+        ax.scatter(proj_PIE_imgs[:, 0], proj_PIE_imgs[:, 1], proj_PIE_imgs[:, 2], s = 10, c = 'c')
+        ax.scatter(proj_MY_imgs[:, 0], proj_MY_imgs[:, 1], proj_MY_imgs[:, 2], s = 15, c = 'r')
         ax.set_xlabel('Principle Axis 1')
         ax.set_ylabel('Principle Axis 2')
         ax.set_zlabel('Principle Axis 3')
@@ -101,7 +85,26 @@ def main():
             ax.imshow(pca_3.components_[i].reshape(img_shape), cmap='gray')
         plt.show()
 
-    # Apply PCA with 40, 80, and 200 Dimensions
+def main():
+    # Display Settings
+    show_pca_result = True      # If we want to plot the PCA results
+    show_num_imgs = 0           # Number of example results to display after done, `0` for no output
+
+    # Set destination paths
+    data_path = '/home/ss/ss_ws/face-recognition/data'
+
+    # Read 500 images from the train set
+    PIE_train, MY_train = readImageData(data_path, set='train', num_PIE_imgs=500)
+
+    # Stack all image vectors together forming X_train set
+    X_train = np.vstack((PIE_train, MY_train))
+    img_shape = np.array([np.sqrt(X_train.shape[1]), np.sqrt(X_train.shape[1])], dtype=int)
+    print(X_train.shape)
+
+    # Apply PCA on 3D (which also included 2D) and visualize results
+    getPCAResults(X_train, PIE_train, MY_train, img_shape, show_plot=show_pca_result)
+
+    # Apply PCA on 40, 80, 200 Dimensions
     Dimensions = [40, 80, 200]
     pca_list = []
     proj_imgs_list = []
