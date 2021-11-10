1ac256038a88301b13384fd5bb273f89187eb09a
SS47816 <ss1057532022@gmail.com>
2021-11-11 03:30:24 +0800

    feat: able to test cnn

--- src/cnn.py
+++ src/cnn.py
@@ -34,7 +34,7 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle
 
 def imshow(img):
     # img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
+    npimg = img.cpu().numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
@@ -63,10 +63,15 @@ class SimpleCNN(nn.Module):
 
         return x
 
+# Train the defined network
+
 lr = 0.001
 n_epochs = 10
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 net = SimpleCNN()
+net.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
 
@@ -75,7 +80,7 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -87,32 +92,71 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
         optimizer.step()
 
         # print statistics
+        report_freq = 100
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % report_freq == report_freq-1:
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+                  (epoch + 1, i + 1, running_loss / report_freq))
             running_loss = 0.0
 
 print('Finished Training')
 
 # Save model weights
-model_path = './cifar_net.pth'
+model_path = os.path.join(repo_path, 'model/cnn.pth')
 torch.save(net.state_dict(), model_path)
 
 # Testing on testset images
 dataiter = iter(testloader)
-images, labels = dataiter.next()
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
+data = dataiter.next()
+images, labels = data[0].to(device), data[1].to(device)
 
 net = SimpleCNN()
 net.load_state_dict(torch.load(model_path))
+net.to(device)
 
 outputs = net(images)
 _, predicted = torch.max(outputs, 1)
 
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(4)))
+# Show images and labels
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+imshow(torchvision.utils.make_grid(images))
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the %d test images: %d %%' % (total, 100 * correct / total))
+
+
+# # prepare to count predictions for each class
+# correct_pred = {classname: 0 for classname in classes}
+# total_pred = {classname: 0 for classname in classes}
+
+# # again no gradients needed
+# with torch.no_grad():
+#     for data in testloader:
+#         images, labels = data
+#         outputs = net(images)
+#         _, predictions = torch.max(outputs, 1)
+#         # collect the correct predictions for each class
+#         for label, prediction in zip(labels, predictions):
+#             if label == prediction:
+#                 correct_pred[classes[label]] += 1
+#             total_pred[classes[label]] += 1
+
+
+# # print accuracy for each class
+# for classname, correct_count in correct_pred.items():
+#     accuracy = 100 * float(correct_count) / total_pred[classname]
+#     print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
+#                                                    accuracy))
